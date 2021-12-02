c10caa70664ef345b2c6556aa4054d3261949eab
Josiah Lansford <josiah.lansford@radiancetech.com>
2021-12-02 17:01:06 -0500

    2.2

---
+++ 2-dive/aim_navigation.py
@@ -0,0 +1,27 @@
+# check for increases in a 3-number sliding window
+
+with open('input.txt') as file:
+  contents = file.read()
+
+# print(contents)
+moves = [x for x in contents.split('\n') if x]
+
+total_distance = 0
+total_depth = 0
+aim = 0
+for move in moves:
+  [direction, distance] = move.split(' ')
+  distance = int(distance)
+  if direction == "forward":
+    total_distance += distance
+    total_depth += distance * aim
+  elif direction == "down":
+    aim += distance
+  elif direction == "up":
+    aim -= distance
+  else:
+    print('parsing error: ', direction, distance)
+print(f'distance: {total_distance}')
+print(f'depth: {total_depth}')
+print(f'product: {total_depth*total_distance}')
+  
